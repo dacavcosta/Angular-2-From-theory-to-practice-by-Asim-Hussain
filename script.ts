d3139058dc26163ec72692607e2d3bd5be43b7fc
turtroll <dcavalcanticosta@gmail.com>
2017-03-03 14:06:38 -0300

    2 - String Interpolation

--- script.ts
+++ script.ts
@@ -6,13 +6,18 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'joke',
     template: `
-        <h1>What did the cheese say when it looked in the mirror?</h1>
-        <p>Halloumi (hello me)</p>
+        <h1>{{setup}}</h1>
+        <p>{{punchline}}</p>
     `
 })
-
 class JokeComponent{
+    setup: string;
+    punchline: string;
 
+    constructor(){
+        this.setup = "What did the cheese say when it looked in the mirror?",
+        this.punchline = "Halloumi (hello me)";
+    }
 }
 
 @NgModule({
@@ -20,7 +25,6 @@ class JokeComponent{
     declarations:[JokeComponent],
     bootstrap:[JokeComponent]
 })
-
 export class AppModule{}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
