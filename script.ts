71f75ba3b0ca1af867ce3142b26c96e44487bf5f
turtroll <dcavalcanticosta@gmail.com>
2017-03-06 15:21:22 -0300

    3 - Looping

--- script.ts
+++ script.ts
@@ -4,26 +4,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'joke',
+    selector: 'joke-list',
     template: `
-        <h1>{{setup}}</h1>
-        <p>{{punchline}}</p>
+        <div class="card card-block"
+            *ngFor="let joke of jokes">
+            <h1 class="card-title">{{joke.setup}}</h1>
+            <p class="card-text">{{joke.punchline}}</p>
+        </div>
     `
 })
-class JokeComponent{
-    setup: string;
-    punchline: string;
+class JokeListComponent{
+    jokes: Object[];
 
     constructor(){
-        this.setup = "What did the cheese say when it looked in the mirror?",
-        this.punchline = "Halloumi (hello me)";
+        this.jokes = [
+            {
+                setup : "What did the cheese say when it looked in the mirror?",
+                punchline : "Halloumi (hello me)"
+            },
+            {
+                setup : "What kind of cheese do you use to disguise a small horse?",
+                punchline : "Mask-a-pony (Mascarpone)"
+            },
+            {
+                setup : "A kid threw a lump of cheddar at me",
+                punchline : "I thought ‘That’s not very mature’"
+            }
+        ];
     }
 }
 
 @NgModule({
     imports:[BrowserModule],
-    declarations:[JokeComponent],
-    bootstrap:[JokeComponent]
+    declarations:[JokeListComponent],
+    bootstrap:[JokeListComponent]
 })
 export class AppModule{}
 
